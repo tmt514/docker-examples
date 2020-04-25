222719820d389e710acffb9812ac475b846573d7
tmt514 <math.tmt514@gmail.com>
2020-04-26 05:45:23 +0800

    dockerfile

--- example-02-duplicating-servers/run.sh
+++ example-02-duplicating-servers/run.sh
@@ -2,5 +2,5 @@
 docker build -t example02 .
 for i in {0..9}
 do
-  docker run -p 1990$i:8080 -e SERVER_NAME="S$i" example02:latest &
+  docker run -p 127.0.0.1:1990$i:8080 -e SERVER_NAME="S$i" example02:latest &
 done
