1bac331a6b9e30c47017a810d32a7c8201784500
tmt514 <math.tmt514@gmail.com>
2020-04-26 05:37:24 +0800

    server.py

--- example-02-duplicating-servers/server/server.py
+++ example-02-duplicating-servers/server/server.py
@@ -10,7 +10,7 @@ SERVER_NAME = os.environ['SERVER_NAME']
 def run_server():
   serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  serversocket.bind(('', PORT))
+  serversocket.bind(('127.0.0.1', PORT))
   serversocket.listen(5)
 
   
@@ -18,22 +18,23 @@ def run_server():
 
   while True:
     (clientsocket, address) = serversocket.accept()
-    json_data = ''
+    clientsocket.send(bytes('[Server {}] Says: Good Morning!\n'.format(SERVER_NAME), 'UTF-8'))
     while True:
-      partial_data = clientsocket.recv(1024)
-      if not partial_data:
+      json_data = clientsocket.recv(1024)
+      if not json_data:
         break
-      json_data += partial_data.decode('UTF-8')
     
-    try:
-      task = json.loads(json_data)
-      task_string = json.dumps(task, indent=2)
-      response = bytes('[Server {}] OK: You have asked about {}\n'.format(SERVER_NAME, task_string), 'UTF-8')
-    except Exception as e:
-      response = bytes('[Server {}] FAILED: JSON parsing error!\n'.format(SERVER_NAME), 'UTF-8')
-      sys.stderr.write('[Server {}] {}\n'.format(SERVER_NAME, e))
+      json_data = str(json_data, 'UTF-8')
+      try:
+        task = json.loads(json_data)
+        task_string = json.dumps(task, indent=2)
+        response = bytes('[Server {}] OK: You have asked about {}\n'.format(SERVER_NAME, task_string), 'UTF-8')
+      except Exception as e:
+        response = bytes('[Server {}] FAILED: JSON parsing error!\n'.format(SERVER_NAME), 'UTF-8')
+        sys.stderr.write('[Server {}] {}\n'.format(SERVER_NAME, e))
 
-    clientsocket.send(response)
+      clientsocket.send(response)
+
     clientsocket.close()
 
 
