91adc866624fa150dbcbeee53bbea193d5e01396
tmt514 <math.tmt514@gmail.com>
2020-04-26 03:38:46 +0800

    Dockerfile for example 02.

--- example-02-duplicating-servers/server/server.py
+++ example-02-duplicating-servers/server/server.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 import socket
 import json
+import sys
+import os
 
 PORT = 8080
+SERVER_NAME = os.environ['SERVER_NAME']
 
 def run_server():
   serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
   serversocket.bind(('', PORT))
   serversocket.listen(5)
-  print('Socket up running at port {}.'.format(PORT))
+
+  
+  sys.stderr.write('[Server {}]: Socket up running at port {}.\n'.format(SERVER_NAME, PORT))
 
   while True:
     (clientsocket, address) = serversocket.accept()
@@ -23,10 +28,10 @@ def run_server():
     try:
       task = json.loads(json_data)
       task_string = json.dumps(task, indent=2)
-      response = bytes('OK: You have asked about {}\n'.format(task_string), 'UTF-8')
+      response = bytes('[Server {}] OK: You have asked about {}\n'.format(SERVER_NAME, task_string), 'UTF-8')
     except Exception as e:
-      response = bytes('FAILED: JSON parsing error!\n', 'UTF-8')
-      print(e)
+      response = bytes('[Server {}] FAILED: JSON parsing error!\n'.format(SERVER_NAME), 'UTF-8')
+      sys.stderr.write('[Server {}] {}\n'.format(SERVER_NAME, e))
 
     clientsocket.send(response)
     clientsocket.close()
